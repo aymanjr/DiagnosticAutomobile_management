8a730e3ac0a3d85b19e830ee1c5c44ae09842d6e
ayman jrm <ayman.jarmoune@outlook.fr>
2022-01-26 12:16:41 +0100

    making the login form

---
+++ login/login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link rel="stylesheet" href="./CSS/style.css">
+    <title>Diagnosstic System</title>
+</head>
+<body>
+
+<form>
+		<h1>Login</h1>
+		<label>Username</label>
+		<input type="text" name="username" />
+		<label>Password</label>
+		<input type="password" name="password" />
+		<br>
+		<div onclick="login()" class="btn">Log In</div>
+	</form>
+
+
+    <script src="/js/jquery.min.js"></script>
+    <script src="/js/popper.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
+    <script src="/js/main.js"></script>
+</body>
+</html>
